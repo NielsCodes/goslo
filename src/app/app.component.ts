55144c46312df81b45c4853d881b69b8745a7b72
Niels Kersic <apps@niels.codes>
2019-11-14 16:28:42 +0100

    Implemented ToneJS player from file input
    File input initiates ToneJS player with reduced playback rate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as Tone from 'tone';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'goslo';
+export class AppComponent implements OnInit {
+
+  buffer;
+  player;
+
+  ngOnInit() {  }
+
+  useAudio() {
+
+    this.player = null;
+
+    const selectedFile = (document.getElementById('audio-input') as HTMLInputElement).files[0];
+    const reader = new FileReader();
+    const audioContext = new AudioContext();
+
+    reader.onloadend = () => {
+      const buffer = reader.result as ArrayBuffer;
+      audioContext.decodeAudioData(buffer, (audioBuffer: AudioBuffer) => {
+        this.player = new Tone.Player(audioBuffer).toMaster();
+        this.player.playbackRate = 0.8;
+        this.player.start();
+      });
+    };
+
+    reader.readAsArrayBuffer(selectedFile);
+
+  }
+
 }
